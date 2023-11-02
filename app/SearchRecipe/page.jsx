6b2b82d2384fe893a7b584b2888226a111915486
gguzmanr <gguzmanr@unal.edu.co>
2023-11-02 10:00:33 -0500

    Creation SearchRecipe

---
+++ app/SearchRecipe/page.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import SearchBar from "./Components/SearchBar";
+import Results from "./Components/Results";
+import Tittle from "./Components/Tittle";
+
+export default function Page(){
+    return(
+        <div>
+            <Tittle/>
+        </div>
+
+    );
+}
+
+
