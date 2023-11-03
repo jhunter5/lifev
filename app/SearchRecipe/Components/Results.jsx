16376548ff001f3791dc8944f2ad3a7f4840a425
gguzmanr <gguzmanr@unal.edu.co>
2023-11-02 22:15:11 -0500

    Search Recipe Filter

--- app/SearchRecipe/Components/Results.jsx
+++ app/SearchRecipe/Components/Results.jsx
@@ -1,1 +1,29 @@
-import React from "react";
+import React from 'react';
+import { Card, Image, CardBody, Input, Button } from "@nextui-org/react";
+
+const Receta = ({ receta }) => {
+  return (
+    <div className="w-full max-w-screen-md mb-4">
+      <Card>
+        <div className="flex">
+          <Image
+            src={receta.imageUrl}
+            alt={receta.name}
+            className="w-96 h-auto"
+          />
+          <CardBody>
+            <h2>{receta.name}</h2>
+            <div>
+              {receta.instructions.split('\n').map((step, i) => (
+                <p key={i}>{step}</p>
+              ))}
+            </div>
+          </CardBody>
+        </div>
+      </Card>
+    </div>
+  );
+};
+
+export default Receta;
+
--- app/SearchRecipe/Components/Tittle.jsx
+++ app/SearchRecipe/Components/Tittle.jsx
@@ -1,18 +1,10 @@
-import React from "react";
+import React from 'react';
 
-export default function Tittle(){
-    return(
-        <div className="content-around bg-black shadow w-full h-full px-10">
-            <div className="w-full h-[71px] bg-teal-300 rounded-[10px]">
-                <p className="w-full h-[71px] text-black text-[40px] text-center font-istok font-semibold">Life V</p>
-            </div>
-            <div className= "content-center w-full h-[57.05px] text-center text-white text-3xl italic font-['Istok Web'] ">More Options </div>
-            <div className="flex justify-evenly flex-wrap items-center bg-stone-800 w-full h-24 px-10">
-                <div className="text-white text-3xl font-istok italic ">Search for:</div>
-                <div className="w-4/5 h-[62px] bg-white rounded-[10px] px-10"></div>
-            </div>
-        </div>
-        
-    )
-}
+const Titulo = () => {
+  return (
+    <div className="text-4xl text-white font-semibold mb-4 font-['Istok Web']">More Recipe Options</div>
+  );
+};
+
+export default Titulo;
 
