0f5877324d8ee74325ead926c37980f37b57774a
bforerob <bforerob@unal.edu.co>
2023-11-06 19:48:00 -0500

    Adding Colors

--- app/ActivePlan/componentsActivePlan/containerAlmuerzo.jsx
+++ app/ActivePlan/componentsActivePlan/containerAlmuerzo.jsx
@@ -21,7 +21,7 @@ const ContainerAlmuerzo = () => {
         },
     ]);
     return (
-        <div className="p-4 grid grid-rows w-fit h-fit bg-none border-x-1 border-teal-300">
+        <div className="p-4 grid grid-rows w-fit h-fit bg-none border-x-2 border-teal-300/60">
             <div className=" align-top text-center w-52 h-12 text-white text-3xl font-normal font-['Istok Web']">Almuerzo</div>
             {almuerzo.map(almuerzo => {
                 return (
