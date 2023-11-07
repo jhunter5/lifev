66d37a0615ac2e0e4181e859216b3c430794189c
bforerob <bforerob@unal.edu.co>
2023-11-06 19:42:09 -0500

    Files delete

--- app/ActivePlan/componentsActivePlan/comida.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Comida() {
-  return (
-    <div className='border-black bg-white w-page h-48 rounded-lg m-5'></div>
-  )
-}
--- app/ActivePlan/componentsActivePlan/titulo.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Titulo() {
-  return (
-    <div>Titulo</div>
-  )
-}
-
-export default Titulo
