7480049d03446eff2c0a63848d824190942ec960
bforerob <bforerob@unal.edu.co>
2023-11-14 20:01:38 -0500

    Corrections

--- app/registerForm/componentsRegisterForm/containerRegisterForm.jsx
+++ app/registerForm/componentsRegisterForm/containerRegisterForm.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import ImageAside from "./imageAside";
 import Form from "./form";
 
 export default function ContainerRegisterForm(props){
--- app/registerForm/componentsRegisterForm/imageAside.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function ImageAside(props){
-    return (
-        <div className=" h-full w-1/2 ">
-            <img className="w-full h-full rounded-l-3xl" src={props.image} alt="register"/>
-        </div>
-    )
-}
