d8c7011189365e8e64e1f464dc9fd097efa30f35
gguzmanr <gguzmanr@unal.edu.co>
2023-11-25 15:17:29 -0500

    connection with the backend of the recipes, and redesign of the page

--- app/SearchRecipe/Components/Results.jsx
+++ app/SearchRecipe/Components/Results.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
-import { Card, Image, CardBody, Input, Button } from "@nextui-org/react";
+import { Card, Image, CardBody, Button } from "@nextui-org/react";
 
 const Receta = ({ receta }) => {
   return (
-    <div className="w-full  mb-4">
+    <div className="w-full mb-4">
       <Card>
-        <div className="flex">
+        <div className="flex bg-gray-200">
           <Image
             isZoomed
             src={receta.image.url}
             alt={"imagen"}
-            className="w-96 h-auto"/>
-          <CardBody>
-            <h2>{receta.label}</h2>
-            <p>Calories: {receta.calories.toFixed(0)}</p>
-            <p>Fat: {receta.fat.toFixed(0)}</p>
-            <p>Carbs: {receta.carbs.toFixed(0)}</p>
-            <p>Protein: {receta.protein.toFixed(0)}</p>
-            {/* <p>{receta.calories}</p> */}
+            className="w-80 h-48"
+          />
+          <CardBody className="p-4">
+            <h2 className="place-self-center text-xl font-bold mb-2">{receta.label}</h2>
+            <p className="text-gray-600 font-istok">Calories: {receta.calories.toFixed(0)}</p>
+            <p className="text-gray-600 font-istok">Fat: {receta.fat.toFixed(0)}g</p>
+            <p className="text-gray-600 font-istok">Carbs: {receta.carbs.toFixed(0)}g</p>
+            <p className="text-gray-600 font-istok">Protein: {receta.protein.toFixed(0)}g</p>
           </CardBody>
+          <Button className=' place-self-end transition ease-in-out delay-150 bg-teal-500 hover:-translate-y-1 hover:scale-110 hover:bg-sky-500 duration-300 w-1/4 h-8 font-istok shadow-lg shadow-black/50 mb-4 mr-4'>View</Button>
         </div>
       </Card>
     </div>
--- app/SearchRecipe/page.jsx
+++ app/SearchRecipe/page.jsx
@@ -1,45 +1,44 @@
-// imports react
 'use client'
 import React, { useState, useEffect } from 'react';
 import { useRouter } from "next/navigation";
+import { Pagination } from "@nextui-org/react";
 
-// imports components
 import Titulo from './Components/Tittle';
 import BarraBusqueda from './Components/SearchBar';
 import Receta from './Components/Results';
-import Pagination from './Components/Pagination';
 import Navbar from "../generalComponents/componentsNavbar/navbar";
 import Footer from "../generalComponents/componentsFooter/footer";
 import LoadingScreen from "../generalComponents/loading.jsx";
 
-// import functions
 import islooged from "../islooged.js";
-
-// const recipes = [
-// ];
 
 const PaginaPrincipal = () => {
   const [searchText, setSearchText] = useState('');
   const [filteredRecipes, setFilteredRecipes] = useState([]);
   const [loading, setLoading] = React.useState(true);
-
+  const [currentPage, setCurrentPage] = useState(1); 
+  const itemsPerPage = 2; 
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const currentRecipes = filteredRecipes.slice(startIndex, endIndex);
+  
 
   const handleSearchChange = async (query) => {
     setSearchText(query);
-
+  
     try {
       const response = await fetch(`https://back-live-v.onrender.com/api/buscanombre?q=${query}`);
       if (response.ok) {
-        const  recetas  = await response.json();
-        const searchedRecipes = Object.values(recetas); // Obtenemos los valores del objeto recetas
+        const recetas = await response.json();
+        const searchedRecipes = Object.values(recetas);
         setFilteredRecipes(searchedRecipes);
+        setCurrentPage(1); 
       }
     } catch (error) {
       console.error('Error fetching recipes:', error);
     }
   };
 
-  // console.log(filteredRecipes);
   const router = useRouter()
 
   useEffect(() => {
@@ -57,16 +56,24 @@ const PaginaPrincipal = () => {
       <Navbar />
       <div className="bg-black w-full h-screen flex items-center justify-center ">
         <div className="flex justify-center w-4/5 h-screen bg-neutral-800 p-10 text-center mx-auto items-center flex-col">
-          <div className="flex flex-col w-auto overflow-x-auto">
+          <div className="flex flex-col w-auto h-auto">
             <Titulo/>
             <BarraBusqueda onSearch={handleSearchChange} />
-            <div className="mt-4 flex flex-col items-center ">
-              {filteredRecipes && filteredRecipes.map((recipe, index) => (
-                <Receta key={index} receta={recipe} />
-                // console.log(recipe)
-              ))}
-            </div>
-            {/* <Pagination /> */}
+            <div className="mt-4 flex flex-col items-center">
+                {currentRecipes.map((recipe, index) => (
+                  <Receta key={index} receta={recipe} />
+                ))}
+              </div>
+              <div className='flex justify-center'>
+              {filteredRecipes.length > itemsPerPage && (
+                <Pagination 
+                  total={10}
+                  current={currentPage}
+                  pageSize={itemsPerPage}
+                  onChange={(page) => setCurrentPage(page)}
+                />
+              )}
+              </div>
           </div>
         </div>
       </div>
