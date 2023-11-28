88be379956616d6dfa5c308cbfce93c65e3feaab
ggallegosr <ggallegosr@unal.edu.co>
2023-11-28 12:31:32 -0500

    change of the typography in profile

--- app/profile/componentsProfile/specifications.jsx
+++ app/profile/componentsProfile/specifications.jsx
@@ -18,29 +18,29 @@ export default function Specifications(userData){
             <div size="sm" className="w-10/12 h-36 bg-zinc-800 rounded-[10px] mt-16 flex flex-row items-center justify-center space-x-12">
                 <div size="sm" className="w-44 h-28 bg-gradient-to-tr from-pink-600 to-rose-600 rounded-[10px] flex flex-col items-center justify-center text-white">
                     <div className="w-56 h-44 mt-0 text-center">
-                    <p className="text-white text-lg font-extrabold font-jost  capitalize mt-5">Diet</p>
-                    <p className="text-white text-lg font-bold font-jost  capitalize text-center mt-5">{userData.userData.dieta}</p>
+                    <p className="text-white text-lg font-extrabold font-sans  capitalize mt-5">Diet</p>
+                    <p className="text-white text-lg font-bold font-sans  capitalize text-center mt-5">{userData.userData.dieta}</p>
                     </div>
                 </div>
 
                 <div size="sm" className="w-44 h-28 bg-gradient-to-tr from-pink-600 to-rose-600 rounded-[10px] flex flex-col items-center justify-center text-white">
                     <div className="w-56 h-28 text-center ml-12 mr-12 mt-0">
-                    <p className="text-white text-lg font-extrabold font-jost  capitalize mt-5">IMC</p>
-                    <p className="text-white text-lg font-bold font-jost  capitalize text-center mt-5">{userData.userData.imc}</p>
+                    <p className="text-white text-lg font-extrabold font-sans  capitalize mt-5">IMC</p>
+                    <p className="text-white text-lg font-bold font-sans  capitalize text-center mt-5">{userData.userData.imc}</p>
                     </div>
                 </div>
 
                 <div size="sm" className="w-44 h-28 bg-gradient-to-tr from-pink-600 to-rose-600 rounded-[10px] flex flex-col items-center justify-center text-white">
                     <div className="w-56 h-28 text-center ml-10 mr-10 mt-0">
-                    <p className="text-white text-lg font-extrabold font-jost  capitalize mt-5">IMC rating </p>
-                    <p className="text-white text-lg font-bold font-jost  capitalize text-center mt-5">{userData.userData.clasificacion_imc}</p>
+                    <p className="text-white text-lg font-extrabold font-sans  capitalize mt-5">IMC rating </p>
+                    <p className="text-white text-lg font-bold font-sans  capitalize text-center mt-5">{userData.userData.clasificacion_imc}</p>
                     </div>
                 </div>
 
                 <div size="sm" className="w-44 h-28 bg-gradient-to-tr from-pink-600 to-rose-600 rounded-[10px] flex flex-col items-center justify-center text-white">
                     <div className="w-56 h-28 text-center ml-10 mr-10 mt-0">
-                    <p className="text-white text-lg font-extrabold font-jost  capitalize mt-5"> Body fat % </p>
-                    <p className="text-white text-lg font-bold font-jost  capitalize text-center mt-5">{userData.userData.pgc}</p>
+                    <p className="text-white text-lg font-extrabold font-sans  capitalize mt-5"> Body fat % </p>
+                    <p className="text-white text-lg font-bold font-sans  capitalize text-center mt-5">{userData.userData.pgc}</p>
                     </div>
                 </div>
             </div>
@@ -54,22 +54,22 @@ export default function Specifications(userData){
             <div size="sm" className="w-10/12 h-48 bg-zinc-800 rounded-[10px] mt-16 flex flex-row items-center justify-center space-x-4">
                 <div size="sm" className="w-72 h-40 bg-teal-300 rounded-[10px] flex flex-col items-center justify-center">
                     <div className="w-56 h-44 mt-5 text-center">
-                    <p className="text-black text-2xl font-extrabold font-jost capitalize mt-5">DAILY CALORIES</p>
-                    <p className="text-black text-2xl font-bold font-jost  capitalize text-center mt-5">{userData.userData.calorias_necesarias_diarias} KCAL</p>
+                    <p className="text-black text-2xl font-extrabold font-sans capitalize mt-5">DAILY CALORIES</p>
+                    <p className="text-black text-2xl font-bold font-sans  capitalize text-center mt-5">{userData.userData.calorias_necesarias_diarias} KCAL</p>
                     </div>
                 </div>
 
                 <div size="sm" className="w-72 h-40 bg-teal-300 rounded-[10px] flex flex-col items-center justify-center">
                     <div className="w-56 h-28 text-center ml-12 mr-12 mt-0">
-                    <p className="text-black text-2xl font-extrabold font-jost  capitalize mb-4">BASAL METABOLISM</p>
-                    <p className="text-black text-2xl font-bold font-jost  capitalize text-center mt-4">{userData.userData.metabolismo_basal} KCAL</p>
+                    <p className="text-black text-2xl font-extrabold font-sans  capitalize mb-4">BASAL METABOLISM</p>
+                    <p className="text-black text-2xl font-bold font-sans  capitalize text-center mt-4">{userData.userData.metabolismo_basal} KCAL</p>
                     </div>
                 </div>
 
                 <div size="sm" className="w-72 h-40 bg-teal-300 rounded-[10px] flex flex-col items-center justify-center">
                     <div className="w-56 h-28 text-center ml-10 mr-10 mt-0">
-                    <p className="text-black text-2xl font-extrabold font-jost  capitalize mb-4">MAINTENANCE CALORIES</p>
-                    <p className="text-black text-2xl font-bold font-jost  capitalize text-center mt-4">{userData.userData.calorias_ideales_diarias} KCAL</p>
+                    <p className="text-black text-2xl font-extrabold font-sans  capitalize mb-4">MAINTENANCE CALORIES</p>
+                    <p className="text-black text-2xl font-bold font-sans  capitalize text-center mt-4">{userData.userData.calorias_ideales_diarias} KCAL</p>
                     </div>
                 </div>
             </div>
@@ -81,21 +81,21 @@ export default function Specifications(userData){
                 <div size= "sm" className=" w-10/12 h-72 bg-zinc-800 rounded-[10px] mt-2 flex flex-row items-center justify-center space-x-4">
                     <div size= "sm" className="w-72 h-64 bg-teal-600 rounded-[10px] mt-3 flex flex-col items-center justify-center">
                         <div className="w-56 h-60 mb-2 text-center">
-                            <p className="text-black text-[15px] font-jost  capitalize text-justify mt-8">
+                            <p className="text-black text-[15px] font-sans  capitalize text-justify mt-8">
                                 You must nourish your body with foods that help your body to function, consuming them is like adjusting the fuel tank to reach your goals and this must be of high quality.
                             </p>
                         </div>
                     </div>
                     <div size= "sm" className="w-72 h-64 bg-teal-600 rounded-[10px] mt-3 flex flex-col items-center justify-center">
                         <div className="w-56 h-60 mb-2 text-center">
-                            <p className="text-black text-[16px] font-jost  capitalize text-justify mt-8">
+                            <p className="text-black text-[16px] font-sans  capitalize text-justify mt-8">
                                 It is the minimum amount of energy your body needs to maintain its vital functions. It is like the essential fuel your body uses to keep you alive, even when you are resting.
                             </p>
                         </div>
                     </div>
                     <div size= "sm" className="w-72 h-64 bg-teal-600 rounded-[10px] mt-3 flex flex-col items-center justify-center">
                         <div className="w-56 h-60 mb-2 text-center">
-                            <p className="text-black text-[16px] font-jost  capitalize text-justify mt-8"> 
+                            <p className="text-black text-[16px] font-sans  capitalize text-justify mt-8"> 
                                 Add up your basal metabolic rate calories and maintain the balance between the calories you burn through daily activities and exercise, and the calories you get from food to maintain your weight without gaining or losing weight.
                             </p>
                         </div>
