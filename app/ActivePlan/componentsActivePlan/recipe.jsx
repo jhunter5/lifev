5a2d764f3bbc6e27acc242b460139d2b0ed36d3e
bforerob <bforerob@unal.edu.co>
2023-11-28 16:18:25 -0500

    corrections

--- app/ActivePlan/componentsActivePlan/recipe.jsx
+++ app/ActivePlan/componentsActivePlan/recipe.jsx
@@ -9,7 +9,7 @@ const Recipe = ({recipe}) => {
         <div className = "w-52 h-fit group bg-white border-1 border-black/70">
             <div className="relative overflow ">
                 <h1 className="text-black text-center">{recipe.recipe.label}</h1>
-                <img src={recipe.recipe.images.SMALL.url} alt={recipe.recipe.label} />
+                <img src={recipe.recipe.image} alt={recipe.recipe.label} />
                 <div className="absolute h-full w-full bg-black/20 flex items-center justify-center -bottom-10 group-hover:bottom-0 opacity-0 group-hover:opacity-100 transition-all duration-300">
                     <Link href={'recipe/'+`${(recipe.recipe.uri).split("_")[1]}`} className="h-full w-full bg-teal-300/20  py-2 px-5 ">
                     <button className="h-full w-full bg-none text-white ">
--- app/ActivePlan/page.jsx
+++ app/ActivePlan/page.jsx
@@ -10,16 +10,45 @@ import obtainToken from "../obtainToken";
 
 
 export default function Page() {
+    const router = useRouter();
+    const [loading, setLoading] = useState(true);
+    const [data, setData] = useState({});    
+    const recipes = data
+    const fetchData = async () => {
+        try {
+          const response = await fetch("https://back-live-v.onrender.com/api/recetas", {
+            headers: {
+              Authorization: obtainToken(), 
+            },
+          });
+          if (response.ok) {
+            const result = await response.json();
+            setData(result);
+            console.log(data);
+            
+          } 
+        } catch (error) {
+          console.error("Error al obtener datos del perfil:", error);
+        }
+      };
     
+      useEffect(() => {
+        if (!islogged()) {
+          router.push('/login')
+        }
+        else {
+          fetchData();
+          setLoading(false)
+        }
+      }, [router]);
 
-
+      
     return (
         <div className='h-full bg-neutral-800'>
             <Navbar />
             <div className='grid place-content-center'>
               <ContainerActivePlan />
             </div>
-            
             <Footer />
         </div>
     )
