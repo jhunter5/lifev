8ac143317d4d35e465b5eef61c8727ebf06e9bd6
bforerob <bforerob@unal.edu.co>
2023-11-28 08:04:33 -0500

    rute changes

--- app/ActivePlan/componentsActivePlan/data.jsx
+++ app/ActivePlan/componentsActivePlan/data.jsx
@@ -3,16 +3,19 @@ export const breakfasts = [
         name: "Ensalada",
         instructions: "1. Lava y corta la lechuga, el tomate y la cebolla.\n2. Mezcla los ingredientes en un tazón.\n3. Agrega aceite de oliva y vinagre al gusto.\n4. Mezcla bien y sirve.",
         imageUrl: "https://images.unsplash.com/photo-1512621776951-a57141f2eefd?auto=format&fit=crop&q=80&w=1770&ixlib=rb-40.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fA%3D%3D",
+        id: "123"
     },
     {
         name: "Sandwich",
         instructions: "1. Extiende la masa de pizza en una bandeja.\n2. Extiende la salsa de tomate sobre la masa.\n3. Agrega el queso, pepperoni, champiñones y aceitunas.\n4. Hornea a 200°C durante 20-25 minutos",
         imageUrl: "https://images.unsplash.com/photo-1528735602780-2552fd46c7af?q=80&w=1473&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        id: "123"
     },
     {
         name: "Lasagna",
         instructions: "1. Extiende la masa de pizza en una bandeja.\n2. Extiende la salsa de tomate sobre la masa.\n3. Agrega el queso, pepperoni, champiñones y aceitunas.\n4. Hornea a 200°C durante 20-25 minutos",
         imageUrl: "https://images.unsplash.com/photo-1546453570-d2fcacdafbb2?q=80&w=1452&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        id: "123"
     },]
 
 
@@ -20,16 +23,19 @@ export const lunches = [
     {
         name: "Pizza",
         instructions: "1. Extiende la masa de pizza en una bandeja.\n2. Extiende la salsa de tomate sobre la masa.\n3. Agrega el queso, pepperoni, champiñones y aceitunas.\n4. Hornea a 200°C durante 20-25 minutos",
+        id: "123",
         imageUrl: "https://images.unsplash.com/photo-1604382354936-07c5d9983bd3?auto=format&fit=crop&q=80&w=1770&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1pYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
         name: "Lasagna",
         instructions: "1. Extiende la masa de pizza en una bandeja.\n2. Extiende la salsa de tomate sobre la masa.\n3. Agrega el queso, pepperoni, champiñones y aceitunas.\n4. Hornea a 200°C durante 20-25 minutos",
+        id: "123",
         imageUrl: "https://images.unsplash.com/photo-1546453570-d2fcacdafbb2?q=80&w=1452&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
         name: "Hamburguesa",
         instructions: "1. Extiende la masa de pizza en una bandeja.\n2. Extiende la salsa de tomate sobre la masa.\n3. Agrega el queso, pepperoni, champiñones y aceitunas.\n4. Hornea a 200°C durante 20-25 minutos",
+        id: "123",
         imageUrl: "https://images.unsplash.com/photo-1571091718767-18b5b1457add?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGhhbWJ1cmd1ZXNhfGVufDB8fDB8fHww"
     },]
 
@@ -37,11 +43,13 @@ export const lunches = [
 export const dinners = [
     {
         name: "Ensalada",
+        id: "123",
         instructions: "1. Lava y corta la lechuga, el tomate y la cebolla.\n2. Mezcla los ingredientes en un tazón.\n3. Agrega aceite de oliva y vinagre al gusto.\n4. Mezcla bien y sirve.",
         imageUrl: "https://images.unsplash.com/photo-1512621776951-a57141f2eefd?auto=format&fit=crop&q=80&w=1770&ixlib=rb-40.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fA%3D%3D",
     },
     {
         name: "Sandwich",
         instructions: "1. Extiende la masa de pizza en una bandeja.\n2. Extiende la salsa de tomate sobre la masa.\n3. Agrega el queso, pepperoni, champiñones y aceitunas.\n4. Hornea a 200°C durante 20-25 minutos",
+        id: "123",
         imageUrl: "https://images.unsplash.com/photo-1528735602780-2552fd46c7af?q=80&w=1473&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },]
--- app/ActivePlan/componentsActivePlan/recipe.jsx
+++ app/ActivePlan/componentsActivePlan/recipe.jsx
@@ -2,9 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 
 
-const redirect = () => {
-    console.log("penepenepenepene")
-}
+
 
 const Recipe = ({recipe}) => {
     return (
@@ -13,8 +11,8 @@ const Recipe = ({recipe}) => {
                 <h1 className="text-black text-center">{recipe.name}</h1>
                 <img src={recipe.imageUrl} alt={recipe.instructions} />
                 <div className="absolute h-full w-full bg-black/20 flex items-center justify-center -bottom-10 group-hover:bottom-0 opacity-0 group-hover:opacity-100 transition-all duration-300">
-                    <Link href={'recipe/'+`${recipe.name}`} className="h-full w-full bg-teal-300/20  py-2 px-5 ">
-                    <button onClick={redirect} className="h-full w-full bg-none text-white ">
+                    <Link href={'recipe/'+`${recipe.id}`} className="h-full w-full bg-teal-300/20  py-2 px-5 ">
+                    <button className="h-full w-full bg-none text-white ">
                         cocinar!
                     </button>
                     </Link>
