f61b471b5e0850e3be886ad5d965a7a18f6ad59d
bforerob <bforerob@unal.edu.co>
2023-11-28 21:22:31 -0500

    asereje a deje

--- app/ActivePlan/componentsActivePlan/recipe.jsx
+++ app/ActivePlan/componentsActivePlan/recipe.jsx
@@ -11,7 +11,7 @@ const Recipe = ({recipe}) => {
                 <h1 className="text-black text-center">{recipe.recipe.label}</h1>
                 <img src={recipe.recipe.image} alt={recipe.recipe.label} />
                 <div className="absolute h-full w-full bg-black/20 flex items-center justify-center -bottom-10 group-hover:bottom-0 opacity-0 group-hover:opacity-100 transition-all duration-300">
-                    <Link href={'recipe/'+`${(recipe.recipe.uri).split("_")[1]}`} className="h-full w-full bg-teal-300/20  py-2 px-5 ">
+                    <Link href={'recipes/'+`${(recipe.recipe.uri).split("_")[1]}`} className="h-full w-full bg-teal-300/20  py-2 px-5 ">
                     <button className="h-full w-full bg-none text-white ">
                     lets cook!!!
                     </button>
